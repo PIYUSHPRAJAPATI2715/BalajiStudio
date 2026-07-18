0cbee29943af9aeea2ffb04b9a7eb6a742a1b1bf
Piyush Prajapati <piyushprajapati2715@gmail.com>
2026-07-18 15:02:08 +0530

    feat: configure Cloudinary API key and secret defaults

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_API_KEY || '191788218421671',
+  api_secret: process.env.CLOUDINARY_API_SECRET || 'dPvGlH1jF7XjNy_Yol_4GPxCRo4',
   secure: true,
 });
 
