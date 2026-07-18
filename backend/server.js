7b44854bb06f67acac00146c6cd2be5ba9cee50a
Piyush Prajapati <piyushprajapati2715@gmail.com>
2026-07-18 13:43:34 +0530

    fix: allow origin true for seamless CORS credentials

--- backend/server.js
+++ backend/server.js
@@ -30,15 +30,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl, Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
